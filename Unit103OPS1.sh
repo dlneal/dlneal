dc44c0355619158177c5b6f8d70ad1f8415b5f74
dneal <dn@dlneal.com>
2024-11-18 19:54:01 -0600

    Unit103OPS1.sh

---
+++ Unit103OPS1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Demetrius BLVCKYODA
+# Date of latest revision: 11/18/2024
+# Purpose: Print a string to the terminal
+echo Hello
