4b1a6eb914f37766d354add0a22456c9c9453ef7
Dlneal <dn@dlneal.com>
2025-01-09 17:49:57 -0600

    301Lab.py

---
+++ 301Lab.py
@@ -0,0 +1,22 @@
+# Write a program that works out whether if a given number is an 
+# odd or even number
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
+
+
+
+
+
+
+
+
+
+# Write a program that works out whether if a given number is an 
+# odd or even number
+number = int(input("Which number do you want to check? "))
+print("This is an even number." if number % 2 == 0 else "This is an odd number.")
